847d7c8e9f557a7c130c4816815770ee72c03c1d
“Vishal” <vishal.naregal@tunexa.com>
2023-10-23 17:18:03 +0530

    ok test read write working

--- libecono/src/main/jni/SerialPort.c
+++ libecono/src/main/jni/SerialPort.c
@@ -24,6 +24,7 @@
 #include <jni.h>
 #include<time.h>
 #include <errno.h>
+#include <termios.h>
 #include "com_flownex_libecono_SerialPort.h"
 
 #include "android/log.h"
@@ -39,9 +40,9 @@ static const char *TAG = "serial_port";
  * Signature: (Ljava/lang/String;II)Ljava/io/FileDescriptor;
  */
 
- #define ERROR -1
- #define Success 0
- #define Timeout -2
+#define ERROR -1
+#define Success 0
+#define Timeout -2
 
 JNIEXPORT jobject JNICALL Java_com_flownex_libecono_SerialPort_open
         (JNIEnv *env, jclass thiz, jstring path, jint baudrate, jint stopBits, jint dataBits,
@@ -49,7 +50,7 @@ JNIEXPORT jobject JNICALL Java_com_flownex_libecono_SerialPort_open
     int fd;
     speed_t speed;
     jobject mFileDescriptor;
-        LOGE("path %d",path);
+    LOGE("path %d",path);
     /* Check arguments */
     {
         speed = baudrate;
@@ -64,8 +65,8 @@ JNIEXPORT jobject JNICALL Java_com_flownex_libecono_SerialPort_open
     {
         jboolean iscopy;
         const char *path_utf = (*env)->GetStringUTFChars(env, path, &iscopy);
-        LOGD("Opening serial port %s with flags 0x%x", path_utf, O_RDWR | flags);
-        fd = open(path_utf, O_RDWR | flags);
+        LOGD("Opening serial port %s with flags 0x%x", path_utf, O_RDWR | O_NOCTTY | O_SYNC);
+        fd = open(path_utf, O_RDWR | O_NOCTTY | O_SYNC);
         LOGD("open() fd = %d", fd);
         (*env)->ReleaseStringUTFChars(env, path, path_utf);
         if (fd == -1) {
@@ -80,7 +81,14 @@ JNIEXPORT jobject JNICALL Java_com_flownex_libecono_SerialPort_open
     {
         struct termios2 cfg;
 
-        ioctl (fd, TCGETS2, &cfg);
+        //ioctl (fd, TCGETS2, &cfg);
+        LOGD("Configuring  port");
+        if (tcgetattr(fd, &cfg))
+        {
+            LOGE("tcgetattr() failed");
+            close(fd);
+            return NULL;
+        }
         // Set baudrate
         cfg.c_cflag &= ~CBAUD;
         cfg.c_cflag |= BOTHER;
@@ -173,13 +181,17 @@ JNIEXPORT jobject JNICALL Java_com_flownex_libecono_SerialPort_open
                 break;
         }
 
-
-        if (ioctl(fd, TCSETS2, &cfg)) {
-            LOGE("tcsets2() failed");
-            close(fd);
-            /* TODO: throw an exception */
+        if (tcsetattr(fd, TCSANOW, &cfg) != 0) {
+            LOGE("Error setting serial port attributes");
             return NULL;
         }
+
+        //   if (ioctl(fd, TCSETS2, &cfg)) {
+        //       LOGE("tcsets2() failed");
+        //       close(fd);
+        //       /* TODO: throw an exception */
+        //       return NULL;
+        //  }
     }
 
     /* Create a corresponding file descriptor */
@@ -195,137 +207,137 @@ JNIEXPORT jobject JNICALL Java_com_flownex_libecono_SerialPort_open
 }
 
 JNIEXPORT jint JNICALL Java_com_flownex_libecono_SerialPort_serial_1read
-  (JNIEnv *env, jobject thiz, jobject fd, jbyteArray buf , jint size , jint T)
-  {
+        (JNIEnv *env, jobject thiz, jobject fd, jbyteArray buf , jint size , jint T)
+{
 
-        jint descriptor = -1;
+    jint descriptor = -1;
 
-               jclass SerialPortClass = (*env)->GetObjectClass(env, thiz);
-              jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
-               jfieldID mFdID = (*env)->GetFieldID(env, SerialPortClass, "mFd", "Ljava/io/FileDescriptor;");
-              jfieldID descriptorID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
-              jobject fd2 = (*env)->GetObjectField(env, thiz, mFdID);
+    jclass SerialPortClass = (*env)->GetObjectClass(env, thiz);
+    jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
+    jfieldID mFdID = (*env)->GetFieldID(env, SerialPortClass, "mFd", "Ljava/io/FileDescriptor;");
+    jfieldID descriptorID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
+    jobject fd2 = (*env)->GetObjectField(env, thiz, mFdID);
 
-              descriptor = (*env)->GetIntField(env, fd2, descriptorID);
-              LOGD("descriptor = %d", descriptor);
+    descriptor = (*env)->GetIntField(env, fd2, descriptorID);
+    LOGD("descriptor = %d", descriptor);
 
-            jbyte *buffer = (*env)->GetByteArrayElements(env, buf, NULL);
+    jbyte *buffer = (*env)->GetByteArrayElements(env, buf, NULL);
 
-            jbyte *resbuf;
-            fd_set set;
+    jbyte *resbuf;
+    fd_set set;
 
-          struct timeval timeout = {0, T};
-          int ready=0;
-          int TotalBytes=0;
-          int r = 0;
-          int expected_Byte=size;
-          int filedisc = descriptor;
-          int sizetoread = 0;
-          static int ntoread=0;
+    struct timeval timeout = {0, T};
+    int ready=0;
+    int TotalBytes=0;
+    int r = 0;
+    int expected_Byte=size;
+    int filedisc = descriptor;
+    int sizetoread = 0;
+    static int ntoread=0;
 
-          LOGD("descriptor2  = %d", descriptor);
+    LOGD("descriptor2  = %d", descriptor);
 
-          FD_ZERO(&set);  /* clear the set */
-          FD_SET(filedisc, &set); /* add our file descriptor to the set */
+    FD_ZERO(&set);  /* clear the set */
+    FD_SET(filedisc, &set); /* add our file descriptor to the set */
 
 
-          double sec = ((double)T/(double)1000000);
-           LOGD(" sec %lf",sec);
+    double sec = ((double)T/(double)1000000);
+    LOGD(" sec %lf",sec);
 
-          ready = select(descriptor + 1 , &set, NULL, NULL, &timeout);
-          LOGD("errno = %s",strerror(errno));
-        	if(ready == -1){
-        	/* Some error has occured in input */
-        	LOGD(" ERROR");
-            		return ERROR;
-        	}
+    ready = select(descriptor + 1 , &set, NULL, NULL, &timeout);
+    LOGD("errno = %s",strerror(errno));
+    if(ready == -1){
+        /* Some error has occured in input */
+        LOGD(" ERROR");
+        return ERROR;
+    }
 
-          	 if(ready == 0){
-        	/* a timeout occured */
-        		LOGD(" TIMEOUT occured");
+    if(ready == 0){
+        /* a timeout occured */
+        LOGD(" TIMEOUT occured");
 
-            		return Timeout;
-        	}
-        	else if(ready){
-        	     clock_t  total_t;
-        			/* there was data to read */
-                 LOGD(" reading ");
+        return Timeout;
+    }
+    else if(ready){
+        clock_t  total_t;
+        /* there was data to read */
+        LOGD(" reading ");
 
-        		 TotalBytes = read(descriptor, buffer, expected_Byte);
-                    LOGD(" first %d", TotalBytes);
-                  if (TotalBytes < expected_Byte){
-                  LOGD(" inside");
-                  sizetoread = expected_Byte - TotalBytes;
-                  ntoread = sizetoread;
+        TotalBytes = read(descriptor, buffer, expected_Byte);
+        LOGD(" first %d", TotalBytes);
+        if (TotalBytes < expected_Byte){
+            LOGD(" inside");
+            sizetoread = expected_Byte - TotalBytes;
+            ntoread = sizetoread;
 
-                  total_t = clock() + sec * CLOCKS_PER_SEC;
-                     while (clock() < total_t) {
+            total_t = clock() + sec * CLOCKS_PER_SEC;
+            while (clock() < total_t) {
 
-                               ntoread = ntoread - r;
-                               double time = ((double)total_t - (double)clock())/ (double)CLOCKS_PER_SEC;
-                               LOGD(" time %lf",time );
-                               LOGD(" ntoread %d",ntoread );
-                               struct timeval t = {0, time};
-                               if(select(descriptor + 1 , &set, NULL, NULL, &t)>0){
-                               r = read(descriptor, &buffer[TotalBytes], ntoread);
-                               LOGD(" r %d", r);
-                               TotalBytes += r;
+                ntoread = ntoread - r;
+                double time = ((double)total_t - (double)clock())/ (double)CLOCKS_PER_SEC;
+                LOGD(" time %lf",time );
+                LOGD(" ntoread %d",ntoread );
+                struct timeval t = {0, time};
+                if(select(descriptor + 1 , &set, NULL, NULL, &t)>0){
+                    r = read(descriptor, &buffer[TotalBytes], ntoread);
+                    LOGD(" r %d", r);
+                    TotalBytes += r;
 
-                               LOGD(" TotalBytes %d", TotalBytes);
+                    LOGD(" TotalBytes %d", TotalBytes);
 
-                               }else{
-                                    LOGD(" break1 ");
-                                     break;
-                                    }
-                               }
-                         }
-                   }
-                   if(TotalBytes == 0){
-                   LOGD("TIMEOUT TOTALBYTE = 0");
-                   return Timeout;
-                   }
-        		(*env)->SetByteArrayRegion(env, buf, 0,size , buffer);
-        	LOGD(" TotalBytes %d", TotalBytes);
-        	 FD_CLR(filedisc, &set);
-        	return TotalBytes;
+                }else{
+                    LOGD(" break1 ");
+                    break;
+                }
+            }
+        }
+    }
+    if(TotalBytes == 0){
+        LOGD("TIMEOUT TOTALBYTE = 0");
+        return Timeout;
+    }
+    (*env)->SetByteArrayRegion(env, buf, 0,size , buffer);
+    LOGD(" TotalBytes %d", TotalBytes);
+    FD_CLR(filedisc, &set);
+    return TotalBytes;
 
-  }
+}
 
 
 JNIEXPORT jint JNICALL Java_com_flownex_libecono_SerialPort_serial_1write
-  (JNIEnv *env , jobject thiz, jobject fd , jbyteArray write_buffer, jint size)
-  {
-     jint descriptor = -1;
+        (JNIEnv *env , jobject thiz, jobject fd , jbyteArray write_buffer, jint size)
+{
+    jint descriptor = -1;
 
-               jclass SerialPortClass = (*env)->GetObjectClass(env, thiz);
-              jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
-              jfieldID mFdID = (*env)->GetFieldID(env, SerialPortClass, "mFd", "Ljava/io/FileDescriptor;");
-              jfieldID descriptorID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
-              jobject fd2 = (*env)->GetObjectField(env, thiz, mFdID);
+    jclass SerialPortClass = (*env)->GetObjectClass(env, thiz);
+    jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
+    jfieldID mFdID = (*env)->GetFieldID(env, SerialPortClass, "mFd", "Ljava/io/FileDescriptor;");
+    jfieldID descriptorID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
+    jobject fd2 = (*env)->GetObjectField(env, thiz, mFdID);
 
-              descriptor = (*env)->GetIntField(env, fd2, descriptorID);
+    descriptor = (*env)->GetIntField(env, fd2, descriptorID);
 
-        jbyte *buffer = (*env)->GetByteArrayElements(env, write_buffer, NULL);
-        LOGD("descriptor = %d", descriptor);
+    jbyte *buffer = (*env)->GetByteArrayElements(env, write_buffer, NULL);
+    LOGD("descriptor = %d", descriptor);
 
-        //unsigned char cmd[] = {0xAA, 0x66, 0x00, 0x04, 0x37, 0x00, 0x3B};
-        int n;
-        n = write(descriptor, buffer, size);
-       // int TotalBytes = read(descriptor, buffer2, 1000);
-        LOGD(" n %d", n);
-        LOGD(" inside write %d", size);
-        if(n != (size))
-        {
-            LOGD(" inside write if %d", n);
-            if(errno == EAGAIN)
+    //unsigned char cmd[] = {0xAA, 0x66, 0x00, 0x04, 0x37, 0x00, 0x3B};
+    int n;
+    n = write(descriptor, buffer, size);
+    // int TotalBytes = read(descriptor, buffer2, 1000);
+    LOGD(" n %d", n);
+    LOGD(" inside write %d", size);
+    if(n != (size))
+    {
+        LOGD(" inside write if %d", n);
+        if(errno == EAGAIN)
             return ERROR;
-        }else{
-            LOGD(" inside write else %d", n);
+    }else{
+        LOGD(" inside write else %d", n);
         (*env)->SetByteArrayRegion(env, write_buffer, 0,size , buffer);
-            return n;
-         }
+        return n;
+    }
 
-  }
+}
 
 
 /*
@@ -334,17 +346,17 @@ JNIEXPORT jint JNICALL Java_com_flownex_libecono_SerialPort_serial_1write
  * Signature: ()V
  */
 JNIEXPORT void JNICALL Java_com_flownex_libecono_SerialPort_close
-        (JNIEnv *env, jobject thiz) {
-    jclass SerialPortClass = (*env)->GetObjectClass(env, thiz);
-    jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
+(JNIEnv *env, jobject thiz) {
+jclass SerialPortClass = (*env)->GetObjectClass(env, thiz);
+jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
 
-    jfieldID mFdID = (*env)->GetFieldID(env, SerialPortClass, "mFd", "Ljava/io/FileDescriptor;");
-    jfieldID descriptorID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
+jfieldID mFdID = (*env)->GetFieldID(env, SerialPortClass, "mFd", "Ljava/io/FileDescriptor;");
+jfieldID descriptorID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
 
-    jobject mFd = (*env)->GetObjectField(env, thiz, mFdID);
-    jint descriptor = (*env)->GetIntField(env, mFd, descriptorID);
+jobject mFd = (*env)->GetObjectField(env, thiz, mFdID);
+jint descriptor = (*env)->GetIntField(env, mFd, descriptorID);
 
-    LOGD("close(fd = %d)", descriptor);
-    close(descriptor);
+LOGD("close(fd = %d)", descriptor);
+close(descriptor);
 }
 
