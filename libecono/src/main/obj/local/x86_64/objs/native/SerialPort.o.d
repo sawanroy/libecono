d9579d54007ede15d2d8768bf03ad69a342ad352
vishalnaregal <vishalnaregal531@gmail.com>
2023-11-27 12:53:44 +0530

    uart complete

---
+++ libecono/src/main/obj/local/x86_64/objs/native/SerialPort.o.d
@@ -0,0 +1,8 @@
+/home/tru-cpu-07/econofinal/libecono/libecono/src/main/obj/local/x86_64/objs/native/SerialPort.o: \
+  /home/tru-cpu-07/econofinal/libecono/libecono/src/main/jni/SerialPort.c \
+  /home/tru-cpu-07/econofinal/libecono/libecono/src/main/jni/termios.h \
+  /home/tru-cpu-07/econofinal/libecono/libecono/src/main/jni/com_flownex_libecono_SerialPort.h
+
+/home/tru-cpu-07/econofinal/libecono/libecono/src/main/jni/termios.h:
+
+/home/tru-cpu-07/econofinal/libecono/libecono/src/main/jni/com_flownex_libecono_SerialPort.h:
